9b151f5e2d86bb3e807338e82059e6df99a3b838
Hyeonsu Choi <hyeonexcel@gmail.com>
2025-05-16 12:45:36 +0900

    fix: 뉴스 제목의 HTML 엔티티 디코딩 처리 추가
    - 뉴스 제목에 포함된 &quot;, &amp; 등의 HTML 엔티티를 사람이 읽기 쉬운 문자로 변환
    - utils.js에 decodeEntities() 함수 추가
    - stripHtml()에서 decodeEntities()를 함께 호출하도록 수정
    - 임베드 메시지 내 뉴스 제목이 실제 기사 제목처럼 출력되도록 개선

--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
+function decodeEntities(text) {
+    return text
+        .replace(/&quot;/g, '"')
+        .replace(/&apos;/g, "'")
+        .replace(/&amp;/g, '&')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&#039;/g, "'"); // 일부 API는 &#039;를 작은따옴표로 사용
+}
+
 function stripHtml(str) {
     return str.replace(/<[^>]*>?/g, '');
 }
